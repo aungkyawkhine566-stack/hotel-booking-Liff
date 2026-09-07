2c18e941bf7f659190b7fa87f8502220dfda797a
aungkyawkhine566-stack <aungkyawkhine566@gmail.com>
2026-09-07 16:04:00 +0630

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -6,7 +6,7 @@ function $(id){ return document.getElementById(id); }
 
 async function init(){
   try{
-    if(CONFIG.LIFF_ID && CONFIG.LIFF_ID !== "YOUR_LIFF_ID"){
+    if(CONFIG.LIFF_ID && CONFIG.LIFF_ID !== "2011476453-A8K9qAG9"){
       await liff.init({liffId: CONFIG.LIFF_ID});
       if(liff.isLoggedIn()){
         profile = await liff.getProfile();
